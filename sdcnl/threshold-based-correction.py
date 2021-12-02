5eee65956b2365ba9cf0e342f180b9708a3945ef
Sean Walsh <seanmichwalsh@protonmail.ch>
2021-12-02 01:23:40 -0500

    Fixes to confidence based correction module of SDCNL pipeline

--- sdcnl/threshold-based-correction.py
+++ sdcnl/threshold-based-correction.py
@@ -5,14 +5,14 @@
 import numpy as np
 import pandas as pd
 
-original_train_labels = pd.read_csv("../data/training-set.csv")["is_suicide"]
-original_test_labels = pd.read_csv("../data/testing-set.csv")["is_suicide"]
+original_train_labels = pd.read_csv("./data/training-set.csv")["is_suicide"]
+original_test_labels = pd.read_csv("./data/testing-set.csv")["is_suicide"]
 
 original_train_labels = np.asarray(original_train_labels)
 original_test_labels = np.asarray(original_test_labels)
 
-predicted_labels = pd.read_csv("../path.csv")["predictions"]
-predicted_probs = pd.read_csv("../path.csv")["probs"]
+predicted_labels = pd.read_csv("./run/prediction_labels.csv")["predictions"]
+predicted_probs = pd.read_csv("./run/prediction_labels.csv")["probs"]
 
 predicted_labels = np.asarray(predicted_labels)
 predicted_probs = np.asarray(predicted_probs)
@@ -20,8 +20,8 @@ predicted_probs = np.asarray(predicted_probs)
 predicted_train_labels = predicted_labels[0:len(original_train_labels)]
 predicted_train_probs = predicted_probs[:len(original_train_labels)]
 
-predicted_test_labels = predicted_labels[len(train_labels):]
-predicted_test_probs = predicted_probs[len(test_labels):]
+predicted_test_labels = predicted_labels[len(original_test_labels):]
+predicted_test_probs = predicted_probs[len(original_test_labels):]
 
 tau = 0.90 # default is 0.90
 
@@ -51,4 +51,5 @@ for i in range(len(original_test_labels)):
 
 final_test_labels = np.asarray(final_test_labels)
 
-print(final_train_labels.shape, final_test_labels.shape)
+# print(final_train_labels.shape, final_test_labels.shape)
+print(final_test_labels)
